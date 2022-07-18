0f3cc3cee19d22a7e079d3402de9aeb8e1fdaef5
Abriel <ravenz1@yandex.ru>
2022-07-18 23:52:42 +0300

    добавид комментов

--- src/Converter.java
+++ src/Converter.java
@@ -4,13 +4,14 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Converter {
-    HashMap<Integer, ArrayList<MonthData>>monthToYearData = new HashMap<>();
-    //HashMap<Integer, ArrayList<CountYear>>yearCount =  new HashMap<>();
-    ArrayList<CountMonth> countMonth = new ArrayList<>();
-    //ArrayList<int[]> countYear= new ArrayList<>();
-    ArrayList<YearData> year = new ArrayList<>();
+    HashMap<Integer, ArrayList<MonthData>>monthToYearData = new HashMap<>();//сюда идут траты и доходы из файлов месяцев
 
-    ArrayList<CountYear> yearCount = new ArrayList<>(); //Тут проблема, он размером 0
+    ArrayList<CountMonth> countMonth = new ArrayList<>(); //сюда идут попарно доход\расход по- месяцам 3шт
+
+    ArrayList<YearData> year = new ArrayList<>(); //тут порубанный год, содержащий месяцы
+
+    ArrayList<CountYear> yearCount = new ArrayList<>(); //сюда идут по-парно доход\расход помесячно за год
+    //Тут проблема, он размером 0
 
 
     void convYear() { // рубим год на части
@@ -21,12 +22,12 @@ public class Converter {
         for (int i = 1; i < lines.length; i++) {
             String line = lines[i];
             String[] parts = line.split(",");
-            int month = Integer.parseInt(parts[0]);
-            int amount = Integer.parseInt(parts[1]);
-            boolean isExpense = Boolean.parseBoolean(parts[2]);
-            year.add(new YearData(month, amount, isExpense));
+            int month = Integer.parseInt(parts[0]);             // собственно то, что пихаем
+            int amount = Integer.parseInt(parts[1]);        // собственно то, что пихаем
+            boolean isExpense = Boolean.parseBoolean(parts[2]); // собственно то, что пихаем
+            year.add(new YearData(month, amount, isExpense)); //пихаем результат в year
         }
-        //  grandFinal();
+
     }
 
     public void converterMonth(){        // рубим месяцы на части...
@@ -38,24 +39,24 @@ public class Converter {
             for(int j = 1; j < lines.length; j++){
                 String line = lines[j];
                 String[] parts = line.split(",");
-                String itemName = parts[0];
-                boolean isExpense = Boolean.parseBoolean(parts[1]);
-                int quantity = Integer.parseInt(parts[2]);
-                int sumOfOne = Integer.parseInt(parts[3]);
+                String itemName = parts[0];                         // собственно то, что пихаем
+                boolean isExpense = Boolean.parseBoolean(parts[1]); // собственно то, что пихаем
+                int quantity = Integer.parseInt(parts[2]);          // собственно то, что пихаем
+                int sumOfOne = Integer.parseInt(parts[3]);      // собственно то, что пихаем
                 MonthData monthData = new MonthData(itemName, isExpense, quantity, sumOfOne);
                 months.add(monthData);
             }
-            monthToYearData.put(i, months);
+            monthToYearData.put(i, months); //пихаем результат в monthToYearData
         }
     }
 
     public void hlamTest(){ // Тут порубанное сортируем по парно Доход и Расход и пихаем в список
         //     countMonth = new ArrayList<>();
         for (Integer data : monthToYearData.keySet()) { // Тут потрошим
-            int incomeMonth = 0;
-            int expensesMonth = 0;
+            int incomeMonth = 0;    //временные переменные по итерационно получают значения произведения количества на цену
+            int expensesMonth = 0;  //временные переменные по итерационно получают значения произведения количества на цену
             for (MonthData set : monthToYearData.get(data)) { // Тут потрошим и выдёргиваем
-                if (set.isExpense) {
+                if (set.isExpense) { //фильтруем доход или расход по типу переменной isExpense, true это расход
                     expensesMonth += set.quantity * set.sumOfOne; // сортируем расходы
                 } else {
                     incomeMonth += set.quantity * set.sumOfOne; // Сортируем доходы
@@ -65,25 +66,25 @@ public class Converter {
         }
 
 
-        int expenses = 0;
-        int income = 0;
+        int expenses = 0;   //временные переменные по итерационно получают значения произведения количества на цену
+        int income = 0;     //временные переменные по итерационно получают значения произведения количества на цену
         for (YearData j : year) { // Потрошим
 
-            if (!j.isExpense) {
+            if (!j.isExpense) { //фильтруем доход или расход по типу переменной isExpense, true это расход
                 income = j.amount; // Сортируем доходы
             } else {
                 expenses = j.amount; // сортируем расходы
             }
 
-            if (!(income == 0) && !(expenses == 0)) { //сортируес, так, что бы идущий в паре с расходом или доходом 0, откинулся и добавился, только расход и доход по-парно в список.
-                //   yearCount.add(income, expenses);
-                yearCount.add(new CountYear(income, expenses)); // при попытке добавится падает
+            if (!(income == 0) && !(expenses == 0)) { //сортируем, так, что бы идущий в паре с расходом или доходом 0, откинулся и добавился, только расход и доход по-парно в список.
+
+                yearCount.add(new CountYear(income, expenses)); //Добавляем по-парно в список доход\расход, за каждый месяц
+
+                // при попытке добавится падает
 
 
             }
         }
-
-
     }
 
 
@@ -166,26 +167,26 @@ public class Converter {
 */
     void monthDataPrint() { //Печатаем максимальные доходы - расходы за каждый месяц
         int a = 1; // стартовый месяц, итератор
-        for (Integer values : monthToYearData.keySet()) {
-
+        for (Integer values : monthToYearData.keySet()) {   // потрошим
+            //Те же временные переменные
             int tempProfit = 0;
             int tempExpense = 0;
             int finExpense = 0;
             int finProfit = 0;
             String Profit = "";
             String Expense = "";
-            for (MonthData  content  : monthToYearData.get(values)) {
+            for (MonthData  content  : monthToYearData.get(values)) {   // потрошим
 
-                if (content.isExpense) {
-                    tempExpense = content.quantity * content.sumOfOne;
-                }  else { tempProfit = content.quantity * content.sumOfOne; }
-                if (finExpense < tempExpense) {
-                    finExpense = tempExpense;
-                    Expense = content.itemName;
+                if (content.isExpense) { //фильтруем
+                    tempExpense = content.quantity * content.sumOfOne;  //вычисляем
+                }  else { tempProfit = content.quantity * content.sumOfOne; }   //вычисляем
+                if (finExpense < tempExpense) { //ищем максимальное значение расхода
+                    finExpense = tempExpense;   //присваиваем максимальное значение
+                    Expense = content.itemName; //присваиваем название
                 }
-                if (finProfit < tempProfit) {
-                    finProfit = tempProfit;
-                    Profit = content.itemName;
+                if (finProfit < tempProfit) {   //ищем максимальное значение дохода
+                    finProfit = tempProfit;     //присваиваем максимальное значение
+                    Profit = content.itemName; //присваиваем название
                 }
             }
             System.out.println("Максимальные доходы за " + a + " месяц " + finProfit + " " + Profit);
@@ -195,33 +196,33 @@ public class Converter {
     }
 
     void yearDataPrint() { //Печатаем максимальные доходы - расходы за год
-        int ourProfit = 0;
+        int ourProfit = 0;  //Те же временные переменные
         int a = 0;
         int b = 0;
         int ourExpense = 0;
         int profit = 0;
         int expence = 0;
-        int[]sumExpense = new int[3];
+        int[]sumExpense = new int[3]; // создаю массивы для расхода и дохода
         int[]sumProfit = new int[3];
         System.out.println("Отчет за 2021 г.");
-        for (YearData content : year) {
-            if (content.isExpense){
-                expence = (content.amount);
-                sumExpense[a++] = expence;
+        for (YearData content : year) {     // потрошим
+            if (content.isExpense){     //фильтруем
+                expence = (content.amount); //приравниваем
+                sumExpense[a++] = expence; // приравниваем, с добавкой к индексу итерируемой переменной
             } else {
-                profit = (content.amount);
-                sumProfit[b++] = profit;
+                profit = (content.amount);  //приравниваем
+                sumProfit[b++] = profit;    // приравниваем, с добавкой к индексу итерируемой переменной
             }
         }
-        for (int z = 1; z < 4; z++) {
+        for (int z = 1; z < 4; z++) {   //печатаю прибыль
             System.out.println("Прибыль за " + z + " месяц составил " + (sumProfit[z - 1] - sumExpense[z - 1]));
 
         }
-        for (int k = 0; k < sumProfit.length; k++) {
+        for (int k = 0; k < sumProfit.length; k++) {    //щитаю медиану
             ourProfit+=sumProfit[k];
         }
 
-        for (int h = 0; h < sumExpense.length; h++) {
+        for (int h = 0; h < sumExpense.length; h++) {   //щитаю медиану
             ourExpense += sumExpense[h];
         }
         System.out.println("Медианный доход за все месяцы " + ourProfit/12);
@@ -232,13 +233,13 @@ public class Converter {
     void grandFinal() { //Тут сверяем счёты
 
         if ((yearCount.isEmpty())) { //проверяем пуст или нет список
-            System.out.println("Данные отсутствуют");
-        } else System.out.println("Данные присутствуют");
+            System.out.println("Данные отсутствуют");   //выдаем юзеру, что он забыл считать данные из п1 и п2 в меню
+        } else System.out.println("Данные присутствуют"); //сообщаем, что огн молодец
         for (int i = 0; i< yearCount .size(); i++) { //потрошим
-            System.out.println(yearCount.get(i).expenses + yearCount.get(i).income);
+            System.out.println(yearCount.get(i).expenses + yearCount.get(i).income); //печатаем доход и расход
             if(yearCount.get(i).expenses != countMonth.get(i).expenses || yearCount.get(i).income != countMonth.get(i).income) { //Собственно сверяем
-                System.out.println("В месяце " + (i+1) + " ошибка");
-            } else  System.out.println("Ошибок нет" + "\n");
+                System.out.println("В месяце " + (i+1) + " ошибка"); //радуем, битыми данными
+            } else  System.out.println("Ошибок нет" + "\n"); //сообщаем, что все ништяк
         }
     }
 
